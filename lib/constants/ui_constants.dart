c9939613cd9e35d718aea751cead6144da4aeb80
Sanush <sanushcj@gmail.com>
2023-03-24 02:01:58 +0530

    colors added

---
+++ lib/constants/ui_constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+String uri = 'http://<yourip>:3000';
+
+class Pallet {
+  // COLORS
+  static final appBarGradient = LinearGradient(
+    colors: [
+      Colors.purple[300]!,
+      Colors.deepPurple[100]!,
+    ],
+    stops: [0.5, 1.0],
+  );
+
+  static final Color? secondaryColor = Colors.deepPurple[200];
+  static final backgroundColor = Colors.purple[100];
+  static const Color greyBackgroundCOlor = Color(0xffebecee);
+  static final selectedNavBarColor = Colors.deepPurple[500];
+  static const unselectedNavBarColor = Colors.black87;
+
+  // STATIC IMAGES
+}
