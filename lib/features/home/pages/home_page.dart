245da373346849e9230e1d305be5f22229f50082
Sanush <sanushcj@gmail.com>
2023-03-30 02:50:57 +0530

    home page added

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:hayaath_shopping/features/auth/controller/user_provider.dart';
 import 'package:provider/provider.dart';
 
