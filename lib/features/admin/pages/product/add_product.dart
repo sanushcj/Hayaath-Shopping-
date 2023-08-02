13c5d410cb771424bb28c93bbb783e366c984008
Sanush <sanushcj@gmail.com>
2023-08-02 16:37:55 +0530

    product image add container added

--- lib/features/admin/pages/product/add_product.dart
+++ lib/features/admin/pages/product/add_product.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:hayaath_shopping/common/alltextfields.dart';
+import 'package:hayaath_shopping/constants/global_variables.dart';
 import 'package:hayaath_shopping/features/admin/pages/product/widgets/addPproduct_screen_widgets.dart';
 import 'package:hayaath_shopping/theme/colors.dart';
 import '../../../../core/utilitiies.dart';
@@ -45,8 +46,6 @@ class _AddProductPageState extends State<AddProductPage> {
     super.dispose();
   }
 
-
-
   void selectImages() async {
     var result = await ProductpickImages();
     setState(() {
@@ -70,102 +69,135 @@ class _AddProductPageState extends State<AddProductPage> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  GestureDetector(
-                    onTap: selectImages,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: DottedBorder(
-                        borderType: BorderType.RRect,
-                        radius: const Radius.circular(10),
-                        dashPattern: const [10, 4],
-                        strokeCap: StrokeCap.round,
-                        child: Container(
-                          width: double.infinity,
-                          height: 200,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(
-                                Icons.folder_open,
-                                size: 40,
-                              ),
-                              const SizedBox(height: 15),
-                              Text(
-                                'Select Product Images',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.grey.shade400,
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    product_images.isNotEmpty
+                        ? SizedBox(
+                            height: 300,
+                            width: double.infinity,
+                            child: ListView.separated(
+                                scrollDirection: Axis.horizontal,
+                                shrinkWrap: true,
+                                itemBuilder: (context, index) => Container(
+                                      height: 300,
+                                      width: 250,
+                                      decoration: BoxDecoration(
+                                          color: Pallete.iceBlue,
+                                          image: DecorationImage(
+                                              fit: BoxFit.contain,
+                                              image: FileImage(
+                                                  product_images[index]))),
+                                    ),
+                                separatorBuilder: (context, index) => rrwidth20,
+                                itemCount: product_images.length),
+                          )
+                        : GestureDetector(
+                            onTap: selectImages,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: DottedBorder(
+                                borderType: BorderType.RRect,
+                                radius: const Radius.circular(10),
+                                dashPattern: const [10, 4],
+                                strokeCap: StrokeCap.round,
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 200,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const Icon(
+                                        Icons.folder_open,
+                                        size: 40,
+                                      ),
+                                      const SizedBox(height: 15),
+                                      Text(
+                                        'Select Product Images',
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.grey.shade400,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ],
+                            ),
                           ),
+                    AddProTextField(
+                      thecontroller: _productnameController,
+                      hint: 'Product Name',
+                      keyboardtype: TextInputType.name,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      height: 50,
+                      width: MediaQuery.of(context).size.width / 2,
+                      decoration: BoxDecoration(
+                        color: Pallete.purpleColor.withOpacity(0.8),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+
+                      //dropdown
+
+                      child: DropdownButton(
+                        borderRadius: BorderRadius.circular(20),
+                        underline: SizedBox(),
+                        dropdownColor: Pallete.iceBlue,
+                        hint: Text('Category'),
+                        value: category,
+                        icon: const Icon(
+                          Icons.keyboard_arrow_down,
+                          color: Pallete.pureblack,
                         ),
+                        items: productCategories.map((String item) {
+                          return DropdownMenuItem(
+                            alignment: Alignment.center,
+                            value: item,
+                            child: NormalText(
+                              thetext: item,
+                              color: Pallete.pureblack,
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (String? newCat) {
+                          setState(() {
+                            category = newCat!;
+                          });
+                        },
                       ),
                     ),
-                  ),
-                  AddProTextField(
-                    controller: _productnameController,
-                    hint: 'Product Name',
-                    keyboardtype: TextInputType.name,
-                  ),
-                  Container(
-                    alignment: Alignment.center,
-                    height: 50,
-                    width: MediaQuery.of(context).size.width / 2,
-                    decoration: BoxDecoration(
-                      color: Pallete.purpleColor.withOpacity(0.8),
-                      borderRadius: BorderRadius.circular(25),
+                    AddProTextField(
+                      thecontroller: _descriptionController,
+                      maxlines: 7,
+                      hint: 'Describe about your product',
                     ),
-                    child: DropdownButton(
-                      borderRadius: BorderRadius.circular(20),
-                      underline: SizedBox(),
-                      dropdownColor: Pallete.iceBlue,
-                      hint: Text('Category'),
-                      value: category,
-                      icon: const Icon(
-                        Icons.keyboard_arrow_down,
-                        color: Pallete.pureblack,
-                      ),
-                      items: productCategories.map((String item) {
-                        return DropdownMenuItem(
-                          alignment: Alignment.center,
-                          value: item,
-                          child: NormalText(
-                            thetext: item,
-                            color: Pallete.pureblack,
-                          ),
-                        );
-                      }).toList(),
-                      onChanged: (String? newCat) {
-                        setState(() {
-                          category = newCat!;
-                        });
-                      },
+                    AddProTextField(
+                      thecontroller: _quantityController,
+                      hint: 'Quantity',
+                      keyboardtype:
+                          TextInputType.numberWithOptions(decimal: false),
                     ),
-                  ),
-                  AddProTextField(
-                    controller: _descriptionController,
-                    maxlines: 7,
-                    hint: 'Describe about your product',
-                  ),
-                  AddProTextField(
-                    controller: _quantityController,
-                    hint: 'Quantity',
-                    keyboardtype:
-                        TextInputType.numberWithOptions(decimal: false),
-                  ),
-                  AddProTextField(
-                    controller: _priceController,
-                    hint: 'Price',
-                    keyboardtype: TextInputType.number,
-                  ),
-                ],
+                    AddProTextField(
+                      thecontroller: _priceController,
+                      hint: 'Price',
+                      keyboardtype: TextInputType.number,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 60,
+                      child: ElevatedButton(
+                          onPressed: () {}, child: NormalText(thetext: 'Sell')),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -174,5 +206,3 @@ class _AddProductPageState extends State<AddProductPage> {
     );
   }
 }
-
-// ignore: must_be_immutable
--- lib/features/admin/pages/product/widgets/addPproduct_screen_widgets.dart
+++ lib/features/admin/pages/product/widgets/addPproduct_screen_widgets.dart
@@ -1,7 +1,5 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
-
 import '../../../../../common/alltextfields.dart';
 import '../../../../../theme/colors.dart';
 
@@ -9,45 +7,42 @@ import '../../../../../theme/colors.dart';
 class AddProTextField extends StatelessWidget {
   AddProTextField(
       {super.key,
-      required this.controller,
+      required this.thecontroller,
       this.maxlines = 1,
       required this.hint,
       this.keyboardtype = TextInputType.text});
 
-  TextEditingController controller = TextEditingController();
+  TextEditingController thecontroller = TextEditingController();
 
   final int maxlines;
   final String hint;
   final TextInputType keyboardtype;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        onSubmitted: (value) => log(
-            '${controller.toString()}  hellooooooooooooooooooooooooooooooooooooooooooooooo'),
-        keyboardType: keyboardtype,
-        maxLines: maxlines,
-        style: TextStyle(color: Pallete.pureblack),
-        controller: controller,
-        decoration: InputDecoration(
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: Pallete.redColor, width: 2),
-          ),
-          hintText: hint,
-          hintStyle: TextStyle(color: Pallete.pureblack),
-          filled: true,
-          fillColor: Pallete.greyColor.withOpacity(0.2),
-          border: OutlineInputBorder(borderSide: BorderSide()),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(width: 2, color: Pallete.premium),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: Pallete.pureblack),
-          ),
+    return TextField(
+      onSubmitted: (value) => log(
+          '${thecontroller.text.toString()}  hellooooooooooooooooooooooooooooooooooooooooooooooo'),
+      keyboardType: keyboardtype,
+      maxLines: maxlines,
+      style: TextStyle(color: Pallete.pureblack),
+      controller: thecontroller,
+      decoration: InputDecoration(
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Pallete.redColor, width: 2),
+        ),
+        hintText: hint,
+        hintStyle: TextStyle(color: Pallete.pureblack),
+        filled: true,
+        fillColor: Pallete.greyColor.withOpacity(0.2),
+        border: OutlineInputBorder(borderSide: BorderSide()),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(width: 2, color: Pallete.premium),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Pallete.pureblack),
         ),
       ),
     );
