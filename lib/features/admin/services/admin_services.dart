5291f7800e33898459d5fba17f7d3270e9492698
Sanush CJ <sanushcj@gmail.com>
2023-09-22 01:09:59 +0530

    product model done

--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -1,1 +1,29 @@
-class AdminServices {}
+import 'dart:io';
+
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:flutter/material.dart';
+import 'package:hayaath_shopping/core/utilitiies.dart';
+
+class AdminServices {
+  sellmyProduct(
+      {required BuildContext context,
+      required String name,
+      required String description,
+      required double price,
+      required double quantity,
+      required String Category,
+      required List<File> img}) async {
+    try {
+      var cloudinary = CloudinaryPublic('duslo3yp6', 'ohfdhtyo');
+      List<String> imageUrls = [];
+      for (var i = 0; i < img.length; i++) {
+        CloudinaryResponse result = await cloudinary.uploadFile(
+          CloudinaryFile.fromFile(img[i].path, folder: name),
+        );
+        imageUrls.add(result.secureUrl);
+      }
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+}
