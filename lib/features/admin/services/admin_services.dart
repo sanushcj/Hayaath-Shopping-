9e022bf73bcf08957aa77a1133205d9ae27a7710
Sanush CJ <sanushcj@gmail.com>
2023-11-03 11:25:57 +0530

    admin

--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
-
 import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:flutter/material.dart';
 import 'package:hayaath_shopping/core/utilitiies.dart';
-import 'package:hayaath_shopping/model/productmodel.dart';
 
 class AdminServices {
   sellmyProduct(
@@ -23,13 +21,6 @@ class AdminServices {
         );
         imageUrls.add(result.secureUrl);
       }
-      ProductModel product = ProductModel(
-          name: name,
-          description: description,
-          quantity: quantity,
-          images: imageUrls,
-          category: Category,
-          price: price);
     } catch (e) {
       showSnackBar(context, e.toString());
     }
